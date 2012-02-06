b780f2571bad4b18b069d3c3347bfd9f1a60f0bd
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-06 17:35:18 -0500

    use html5 validators

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -79,4 +79,4 @@
 # You can opt-in to new HTML5 browser validations (for things like email and url inputs) by setting
 # this to false. Doing so will add a `novalidate` attribute to the `<form>` tag.
 # See http://diveintohtml5.org/forms.html#validation for more info.
-# Formtastic::FormBuilder.perform_browser_validations = true
+Formtastic::FormBuilder.perform_browser_validations = true
