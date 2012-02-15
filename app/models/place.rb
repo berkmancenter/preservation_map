39a356182d3ee494a09acf5c7270088f4b442cc0
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-14 19:07:32 -0500

    make sure abbr is in there because we now depend on it to identify unique locations in the CSV

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,6 @@ class Place < ActiveRecord::Base
     has_many :place_measures
     has_many :measures, :through => :place_measures
     belongs_to :geo_graph
+
+    validates :api_abbr, :presence => true
 end
