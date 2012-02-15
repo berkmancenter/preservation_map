2cec4d821b310d178c542f887c121f0ad339e2df
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-14 19:09:43 -0500

    added more fields, got to_json working, added defaults, rewrote import

--- app/models/geo_graph.rb
+++ app/models/geo_graph.rb
@@ -1,38 +1,58 @@
 require 'csv'
+require 'json'
 
 class GeoGraph < ActiveRecord::Base
     belongs_to :user
+
+    has_many :places
+    has_many :measures
     has_and_belongs_to_many :external_data_sources
-    has_many :place_measures, :order => [:place_id, :measure_id]
-    has_many :places, :through => :place_measures, :uniq => true, :order => :id
-    has_many :measures, :through => :place_measures, :uniq => true, :order => :id
     has_attached_file :import_data
+
     validates_attachment_content_type :import_data, :content_type => 'text/csv'
     validates_attachment_size :import_data, :in => 1..1.megabyte
     validates :name, :presence => true, :length => { :in => 3..50 }
+
+    serialize :color_theme
+
+    before_save :add_defaults
 
     def import_from_attachment!
         place_col_names = Code::Application.config.place_column_names
 
         if places? and measures?
             table.each do |row|
-                place = Place.new(
+                places << Place.new(
                     :name => row[place_col_names[:name]],
                     :latitude => row[place_col_names[:latitude]],
                     :longitude => row[place_col_names[:longitude]],
                     :api_abbr => row[place_col_names[:api_abbr]]
                 )
-                places << place
+            end
+
+
+            table.headers.each do |header|
+                unless place_col_names.value? header
+                    measures << Measure.new(:name => header)
+                end
+            end
+
+            self.save
+
+            table.each do |row|
+                place = places.find_by_api_abbr(row[place_col_names[:api_abbr]])
                 row.each do |column_name, value|
                     unless place_col_names.value? column_name
-                        measure = Measure.find_or_create_by_name_and_geo_graph_id(column_name, id)
-                        place_measure = PlaceMeasure.new(:value => value)
+                        measure = measures.find_by_name(column_name)
+                        place_measure = PlaceMeasure.new(:value => value.to_f)
                         measure.place_measures << place_measure
                         place.place_measures << place_measure
-                        place_measures << place_measure
                     end
                 end
             end
+
+            self.color_measure_id = self.measures[3].id
+            self.size_measure_id = self.measures[3].id
         end
     end
 
@@ -54,6 +74,41 @@ class GeoGraph < ActiveRecord::Base
     end
 
     def all_measures
-        measures + external_data_sources.map { |source| source.measures }.flatten!
+        unless external_data_sources.empty?
+            measures + external_data_sources.map { |source| source.measures }.flatten!
+        else
+            measures
+        end
+    end
+
+    def to_json
+        return_object = [];
+
+        places.each do |place|
+            return_object << {
+                :name => place.name,
+                :latitude => place.latitude,
+                :longitude => place.longitude,
+                :size => Measure.find(size_measure_id).size(place),
+                :color => Measure.find(color_measure_id).color(place),
+                :colorMeasureValue => Measure.find(color_measure_id).value(place),
+                :sizeMeasureValue => Measure.find(size_measure_id).value(place)
+            }
+        end
+
+        return return_object.to_json
+    end
+
+    protected
+    def add_defaults
+        self.color_theme ||= {
+            0 => '#0000b0',
+            25 => '#00e3eb',
+            50 => '#00d100',
+            75 => '#ffff00',
+            100 => '#e80202'
+        }
+        self.max_spot_size ||= 20 
+        self.min_spot_size ||= 3
     end
 end
