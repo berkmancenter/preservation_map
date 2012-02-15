bb15a3d24c34666208afa5ca3170d6b8d9a8a896
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-14 19:00:43 -0500

    moved color theme into geograph

--- app/models/color_theme.rb
+++
@@ -1,2 +0,0 @@
-class ColorTheme < ActiveRecord::Base
-end
--- db/migrate/20120201213217_create_color_themes.rb
+++
@@ -1,9 +0,0 @@
-class CreateColorThemes < ActiveRecord::Migration
-  def change
-    create_table :color_themes do |t|
-      t.string :gradient
-
-      t.timestamps
-    end
-  end
-end
