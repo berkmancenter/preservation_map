2407923ca2c6dd345e03b2fbb756a7b2c146deba
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-14 19:00:04 -0500

    moved color theme in here and added other configurable bits

--- db/migrate/20120201213129_create_geo_graphs.rb
+++ db/migrate/20120201213129_create_geo_graphs.rb
@@ -3,6 +3,11 @@ class CreateGeoGraphs < ActiveRecord::Migration
     create_table :geo_graphs do |t|
       t.string :name
       t.integer :user_id
+      t.integer :color_measure_id
+      t.integer :size_measure_id
+      t.integer :max_spot_size
+      t.integer :min_spot_size
+      t.text :color_theme
       t.has_attached_file :import_data
 
       t.timestamps
