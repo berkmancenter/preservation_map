5e9a256cf26ed6cba383a1d533fc76968a0f51ba
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-14 19:10:53 -0500

    added method to figure out where each datapoint lies wrt others

--- app/models/place_measure.rb
+++ app/models/place_measure.rb
@@ -1,5 +1,11 @@
 class PlaceMeasure < ActiveRecord::Base
     belongs_to :place
     belongs_to :measure
-    belongs_to :geo_graph
+    validates :value, :place, :measure, :presence => true
+
+    def to_percent
+        min_value = PlaceMeasure.where( :measure_id => measure_id ).minimum(:value)
+        max_value = PlaceMeasure.where( :measure_id => measure_id ).maximum(:value)
+        return (value - min_value) / (max_value - min_value)
+    end
 end
