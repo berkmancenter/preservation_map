78bdf7af39bd2f4c93b6e4714b4565c2af43c64c
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-24 15:51:31 -0500

    redirect to view on create (because we can set defaults from there now) and respond with nothing when updated via ajax

--- app/controllers/geo_graphs_controller.rb
+++ app/controllers/geo_graphs_controller.rb
@@ -16,7 +16,7 @@ class GeoGraphsController < ApplicationController
         @geograph.import_data_from_external_sources!
         respond_to do |format|
             if @geograph.save
-                format.html {redirect_to edit_geo_graph_path(@geograph)}
+                format.html {redirect_to geo_graph_path(@geograph)}
             else
                 flash[:alert] = 'Could not add that GeoGraph.'
                 format.html { render 'new' }
@@ -57,8 +57,13 @@ class GeoGraphsController < ApplicationController
         end
         respond_to do |format|
             if @geograph.update_attributes(params[:geo_graph])
-              format.html  { redirect_to(@geograph,
-                            :notice => 'Post was successfully updated.') }
+              format.html  {
+                  if request.xhr?
+                      head :no_content
+                  else
+                      redirect_to(@geograph, :notice => 'Post was successfully updated.')
+                  end
+              }
               format.json  { head :no_content }
             else
               format.html  { render "edit" }
