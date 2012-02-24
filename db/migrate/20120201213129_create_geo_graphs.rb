1eef11ac2cb50dca8559f00afd80dcb25aa393f1
Justin Clark <jclark@cyber.law.harvard.edu>
2012-02-24 14:36:52 -0500

    add default view stuff to geograph

--- db/migrate/20120201213129_create_geo_graphs.rb
+++ db/migrate/20120201213129_create_geo_graphs.rb
@@ -11,6 +11,9 @@ class CreateGeoGraphs < ActiveRecord::Migration
       t.integer :num_legend_colors
       t.integer :color_theme_id
       t.integer :num_zoom_levels
+      t.integer :default_zoom_level
+      t.float :default_latitude
+      t.float :default_longitude
       t.has_attached_file :import_data
 
       t.timestamps
